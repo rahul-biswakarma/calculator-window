1f8163d1a2711ba1ba292b42b7b7e193e880ccc2
Rahul Biswakarma <rahul.id39@gmail.com>
2023-01-22 16:15:24 +0530

    window drag system works

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 import Window from "./components/Window";
 
 const App: React.FC = () => {
-  type WindowPosition = {
-    x: number;
-    y: number;
-  };
+	type WindowPosition = {
+		x: number;
+		y: number;
+	};
 
-  const [windowPosition, setWindowPosition] = useState<WindowPosition>({
-    x: 0,
-    y: 0,
-  });
-  return (
-    <div className="relative min-h-[100vh] h-full w-full">
-      <Window
-        windowPosition={windowPosition}
-        setWindowPosition={setWindowPosition}
-      />
-    </div>
-  );
+	const [windowPosition, setWindowPosition] = useState<WindowPosition>({
+		x: 0,
+		y: 0,
+	});
+
+	let parentRef = useRef<HTMLDivElement>(null);
+
+	return (
+		<div
+			ref={parentRef}
+			className="relative min-h-[100vh] h-full w-full overflow-hidden select-none"
+		>
+			<Window
+				windowPosition={windowPosition}
+				setWindowPosition={setWindowPosition}
+				parentRef={parentRef}
+			/>
+		</div>
+	);
 };
 
 export default App;
--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -1,21 +1,69 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 type WindowPosition = {
-  x: number;
-  y: number;
+	x: number;
+	y: number;
 };
 
 const Window: React.FC<{
-  windowPosition: WindowPosition;
-  setWindowPosition: React.Dispatch<React.SetStateAction<WindowPosition>>;
-}> = ({ windowPosition, setWindowPosition }) => {
-  return (
-    <div
-      className={`absolute top-[${windowPosition.x}px] left-[${windowPosition.y}px]`}
-    >
-      Window
-    </div>
-  );
+	windowPosition: WindowPosition;
+	setWindowPosition: React.Dispatch<React.SetStateAction<WindowPosition>>;
+	parentRef: React.RefObject<HTMLDivElement>;
+}> = ({ windowPosition, setWindowPosition, parentRef }) => {
+	var windowRef = useRef<HTMLDivElement>(null);
+
+	let X: number, Y: number;
+
+	const [isMoving, setIsMoving] = useState(false);
+	const [initialX, setInitialX] = useState(0);
+	const [initialY, setInitialY] = useState(0);
+	const [finalX, setFinalX] = useState(0);
+	const [finalY, setFinalY] = useState(0);
+
+	const handleMouseDown = (e: React.MouseEvent) => {
+		setIsMoving(true);
+		setInitialX(e.clientX);
+		setInitialY(e.clientY);
+	};
+
+	const handleMouseMove = (e: React.MouseEvent) => {
+		if (parentRef && windowRef.current && isMoving && parentRef.current) {
+			const maxX =
+				parentRef.current.offsetWidth - windowRef.current.offsetWidth;
+			const maxY =
+				parentRef.current.offsetHeight - windowRef.current.offsetHeight;
+			windowRef.current.style.top = `${Math.min(
+				Math.max(windowPosition.y + (e.clientY - initialY), 0),
+				maxY
+			)}px`;
+			windowRef.current.style.left = `${Math.min(
+				Math.max(windowPosition.x + (e.clientX - initialX), 0),
+				maxX
+			)}px`;
+			setFinalX(windowPosition.x + (e.clientX - initialX));
+			setFinalY(windowPosition.y + (e.clientY - initialY));
+		}
+	};
+
+	const handleMouseUp = () => {
+		setIsMoving(false);
+		setInitialX(finalX);
+		setWindowPosition({ x: finalX, y: finalY });
+	};
+
+	return (
+		<div ref={parentRef}>
+			<div
+				className={`absolute top-[${windowPosition.y}px] left-[${windowPosition.x}px] w-[300px] h-[300px] bg-[#000]/40 backdrop-blur-3xl border-[1px] border-[#49494e] border-radius-[5px]`}
+				ref={windowRef}
+				onMouseDown={handleMouseDown}
+				onMouseMove={handleMouseMove}
+				onMouseUp={handleMouseUp}
+			>
+				Window
+			</div>
+		</div>
+	);
 };
 
 export default Window;
